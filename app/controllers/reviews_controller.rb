474db3fb4d47bb7af4c2769312d28c296b7a9a96
kirill ginko <kirillginko@gmail.com>
2020-01-29 18:32:58 -0500

    review routes added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
     else
       render :new, notice: 'your review wasnt saved'
     end
@@ -17,11 +17,13 @@ class ReviewsController < ApplicationController
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+  def review_params
+    params.require(:review).permit(
+      :content, :rating
+    )
   end
 
-  def review_params
-    params.require(:review).permit(:content, :rating)
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: 'new_restaurant_review'
+  get 'restaurants/:restaurant_id/reviews', to: 'reviews#index', as: 'reviews'
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: 'restaurant_reviews'
 end
