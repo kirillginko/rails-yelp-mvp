156eba8de9d25bfbba2d7fa11b6fb394b871e307
kirill ginko <kirillginko@gmail.com>
2020-01-30 14:46:01 -0500

    delete partials folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'restaurants#index'
   resources :restaurants
   get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: 'new_restaurant_review'
   get 'restaurants/:restaurant_id/reviews', to: 'reviews#index', as: 'reviews'
